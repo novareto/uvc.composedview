38bfb80b08b8b1e93952aef8d683f07afc78696b
goschtl <ck@novareto.de>
2016-11-07 10:23:12 +0100

    add an available method for evaluating if the view should be shown

--- src/uvc/composedview/components.py
+++ src/uvc/composedview/components.py
@@ -70,17 +70,19 @@ def get_tabs(view, update=True):
         # This security check is crap
         # It's due to Grok protecting only __call__
         # This works as long as we don't have a security proxy.
-        if check_security(tab, '__call__'):
-            if update is True:
-                stopper = tab.update()
-                if stopper is None:
-                    if IForm.providedBy(tab):
-                        stopper = tab.updateForm()
-                        if stopper is not None:
-                            raise StopperException(stopper)
-                else:
-                    raise StopperException(stopper)
-            tabs[id] = tab
+        available = getattr(tab, 'available', True)
+        if available:
+            if check_security(tab, '__call__'):
+                if update is True:
+                    stopper = tab.update()
+                    if stopper is None:
+                       if IForm.providedBy(tab):
+                            stopper = tab.updateForm()
+                            if stopper is not None:
+                                raise StopperException(stopper)
+                    else:
+                        raise StopperException(stopper)
+                tabs[id] = tab
     return tabs
 
 
@@ -92,6 +94,8 @@ class ComposedPage(Page):
     updating = True
     template = TALTemplate(os.path.join(
         os.path.dirname(__file__), 'composed.cpt'))
+
+    available = True
 
     @property
     def title(self):
@@ -130,3 +134,4 @@ class ComposedPage(Page):
         """Returns the URL of a tab.
         """
         return self.url(self, name=id)
+
